0e8b63a4727345f66752a25fffcc40c51e8549f0
singh7arun <82437821+singh7arun@users.noreply.github.com>
2026-06-28 23:19:21 +0530

    Add: search-2d-matrix - submission-4

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-4.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i = getRowIndex(matrix, target);
+        int j = 0;
+        
+        while (j < matrix[0].length && matrix[i][j] <= target){
+            if (matrix[i][j] == target){
+                return true;
+            }
+            j++;
+        }
+        return false;
+    }
+
+    public int getRowIndex(int[][] matrix, int target){
+        int i = 0;
+
+        while (i < matrix.length-1 && matrix[i+1][0] <= target){
+            i++;
+        }
+        return i;
+    }
+}
