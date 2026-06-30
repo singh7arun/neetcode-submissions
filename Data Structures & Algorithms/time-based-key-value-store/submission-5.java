f96fbcd0d8f8b8ee0c79244202e250d035042488
singh7arun <82437821+singh7arun@users.noreply.github.com>
2026-06-30 22:10:36 +0530

    Add: time-based-key-value-store - submission-5

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-5.java
@@ -0,0 +1,32 @@
+class TimeMap {
+
+    Map<String, TreeMap<Integer, String>> timeMap;
+
+    public TimeMap() {
+        timeMap = new HashMap<>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        TreeMap<Integer, String> map = timeMap.get(key);
+        if (Objects.isNull(map)){
+            map = new TreeMap<>();
+            timeMap.put(key, map);
+        }
+        map.put(timestamp, value);
+        
+    }
+    
+    public String get(String key, int timestamp) {
+        TreeMap<Integer, String> map = timeMap.get(key);
+        if (map != null){
+            String val = map.get(timestamp);
+            if (val != null){
+                return val;
+            } else {
+                Map.Entry<Integer, String> entry = map.floorEntry(timestamp);
+                return Objects.nonNull(entry) ? entry.getValue() : "";
+            }
+        }
+        return "";
+    }
+}
